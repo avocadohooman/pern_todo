cf83fbcbb4173600eff629b64cb78e43480f3dcf
gerhard.molin <gmolin@student.hive.fi>
2021-05-07 10:47:46 +0300

    adding github actions for CI/CD

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 const app = express();
-const cors = require('cors');
 
 app.use(express.json());
-app.use(cors());
 
 const PORT = 3000;
 
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
-})
+});
