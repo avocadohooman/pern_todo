24059444a84141a7f6966c9a0aadee10a959383e
gerhard.molin <gmolin@student.hive.fi>
2021-05-11 15:46:30 +0300

    adding client side: adding todo component + adding CROS policy

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+//components 
+import InputTodo from './components/inputTodo';
+
+const App = () => {
+
+
+  return (
+    <div className="container">
+      <InputTodo/>
+    </div>
+  )
+}
+
+export default App;
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import Pool from './db';
 import todoRouter from './controllers/todoRouter';
+import cors from 'cors';
 
 const app = express();
 const pool = Pool;
 
+app.use(cors());
 app.use(express.json());
 app.use('/todo', todoRouter);
 
