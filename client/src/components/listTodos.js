1875053b22bb5105169a48e7138f1b699f205561
gerhard.molin <gmolin@student.hive.fi>
2021-05-11 16:42:21 +0300

    adding delete component

--- client/src/components/listTodos.js
+++ client/src/components/listTodos.js
@@ -12,7 +12,20 @@ const ListTodos = () => {
         } catch (error) {
             console.log(error.message);
         }
-    }
+    };
+
+    const deleteTodo = async (id) => {
+        try {
+            const response = await fetch(`http://localhost:3000/todo/${id}`, {
+                method: "DELETE"
+            });
+            console.log("Deleted", response);
+            const cleanedTods = todos.filter(todo => todo.todo_id !== id);
+            setToDos(cleanedTods);
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
 
     useEffect(() => {
         getTodos();
@@ -32,7 +45,7 @@ const ListTodos = () => {
             <tr key={todo.todo_id}>
                 <td>{todo.description}</td>
                 <td>Edit</td>
-                <td>Delete</td>
+                <td><button onClick={() => deleteTodo(todo.todo_id)} className="btn btn-danger">Delete</button></td>
             </tr>
         ))}
         </tbody>
