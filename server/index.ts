864629a1b3a59080fb73d841c9c3263081fb58b4
gerhard.molin <gmolin@student.hive.fi>
2021-05-14 12:01:42 +0300

    adding process.env to port

--- server/index.ts
+++ server/index.ts
@@ -12,7 +12,7 @@ app.use(express.static('build'));
 app.use('/todo', todoRouter);
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
 });
