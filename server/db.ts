9abedc43a3c8ff154cde72cb907ed19eaba0c437
gmolin <gmolin@student.hive.fi>
2021-05-18 19:13:53 +0300

    deploying

--- server/db.ts
+++ server/db.ts
@@ -11,7 +11,7 @@ require('dotenv').config();
 
 // const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 const proConfig = process.env.DATABASE_URL; //heroku addons
-const devConfig = 'postgres://bjyadbbntzltdy:e6c8a74215477e9a09065afa51956d71e8cc55f5ef030dffa0b6c21036e32783@ec2-176-34-105-15.eu-west-1.compute.amazonaws.com:5432/d3q10p0cudft6b';
+const devConfig = process.env.DATABASE_URL;
 
 const testConfig = {
 
