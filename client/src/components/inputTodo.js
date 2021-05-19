a6070133a9b6ba412f598817a6948b39d7634ac8
gerhard.molin <gmolin@student.hive.fi>
2021-05-19 17:00:58 +0300

    deploying

--- client/src/components/inputTodo.js
+++ client/src/components/inputTodo.js
@@ -22,7 +22,7 @@ const InputTodo = () => {
     }
     return (
         <>
-            <h1 className="text-center mt-5">input field</h1>
+            <h1 className="text-center mt-5">ToDo App</h1>
             <form className="d-flex mt-5" onSubmit={onSubmitForm}> 
                 <input type="text"
                 id="newTodo" 
