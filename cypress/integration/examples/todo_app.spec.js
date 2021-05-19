a78210c5c076eb70f38822331f13cb7b111ddaec
gerhard.molin <gmolin@student.hive.fi>
2021-05-19 17:09:32 +0300

    deploying

--- cypress/integration/examples/todo_app.spec.js
+++ cypress/integration/examples/todo_app.spec.js
@@ -1,7 +1,7 @@
 describe('Todo app is online', function() {
     it('front-page can be opened', function() {
         cy.visit('http://localhost:3000');
-        cy.contains('input field');
+        cy.contains('ToDo App');
     })
 })
 
