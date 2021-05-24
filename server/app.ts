f01597a75eec1195dd2ddafb21320c250eef8f5b
gerhard.molin <gmolin@student.hive.fi>
2021-05-24 17:51:58 +0300

    deploying

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,20 @@
 import express from 'express';
 import todoRouter from './controllers/todoRouter';
 import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
 app.use(express.static('build'));
-app.use('/todo', todoRouter);
+
+const limit = rateLimit({
+    max: 10,// max requests
+    windowMs: 60 * 60 * 1000, // 1 Hour
+    message: 'Too many requests' // message to send
+});
+app.use('/todo', todoRouter, limit);
 
 app.get('/health', async (req, res) => {
     res.send('ok');
